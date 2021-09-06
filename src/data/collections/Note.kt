51baa4f9d0a754eb3de457588ee6fb5ca0cc639c
nehabalaji <nehabalaji10@gmail.com>
2021-09-06 12:41:07 +0530

    add note and user collections

---
+++ src/data/collections/Note.kt
@@ -0,0 +1,13 @@
+package com.notesapp.data.collections
+
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+
+data class Note(
+    val title: String,
+    val content: String,
+    val date: Long,
+    val owners: List<String>,
+    @BsonId
+    val id: String = ObjectId().toString()
+)
