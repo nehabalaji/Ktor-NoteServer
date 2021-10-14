d5140cab6af9b4e549a8a065564b79526b19ff8a
nehabalaji <nehabalaji10@gmail.com>
2021-10-14 19:50:49 +0530

    Get note for a user

--- src/Application.kt
+++ src/Application.kt
@@ -1,10 +1,13 @@
 package com.notesapp
 
+import com.notesapp.data.checkPasswordForEmail
 import com.notesapp.data.collections.User
 import com.notesapp.data.registerUser
 import com.notesapp.routes.loginRoute
+import com.notesapp.routes.noteRoutes
 import com.notesapp.routes.registerRoute
 import io.ktor.application.*
+import io.ktor.auth.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.response.*
@@ -21,14 +24,30 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.module(testing: Boolean = false) {
     install(DefaultHeaders) //(optional) causes ktor to append all the extra info to all responses that come from our server
     install(CallLogging) //(optional) logs all the http requests that comes to the server and the responses
-    install(Routing) {
-        registerRoute()
-        loginRoute()
-    } //makes sure we can define url endpoints where clients can connect to
     install(ContentNegotiation){
         gson {
             setPrettyPrinting()
         }
     } //interprets content that is sent to/from the server (here json)
+    install(Authentication) {
+        configAuth()
+    }
+    install(Routing) {
+        registerRoute()
+        loginRoute()
+        noteRoutes()
+    } //makes sure we can define url endpoints where clients can connect to
 }
 
+private fun Authentication.Configuration.configAuth() {
+    basic {
+        realm = "Note Server" //Name of the server or something that would pop up in a browser
+        validate { credentials ->
+            val email = credentials.name
+            val password = credentials.password
+            if (checkPasswordForEmail(email, password)) {
+                UserIdPrincipal(email) //Kind of a type of users to keep track of who is authenticated or not
+            } else null
+        }  //check name and password by defining a logic
+    }
+}
---
+++ src/routes/NoteRoute.kt
@@ -0,0 +1,25 @@
+package com.notesapp.routes
+
+import com.notesapp.data.getNotesForUser
+import io.ktor.application.call
+import io.ktor.auth.UserIdPrincipal
+import io.ktor.auth.authenticate
+import io.ktor.auth.principal
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.HttpStatusCode.Companion.OK
+import io.ktor.response.respond
+import io.ktor.routing.Route
+import io.ktor.routing.get
+import io.ktor.routing.route
+
+fun Route.noteRoutes() {
+    route("/getNotes") {
+        authenticate {
+            get {
+                val email = call.principal<UserIdPrincipal>()!!.name
+                val notes = getNotesForUser(email)
+                call.respond(OK, notes)
+            }
+        } //user must be authenticated in order to access the note
+    }
+}
